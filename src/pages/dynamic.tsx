9da04d7e01a6a4c1e9eb86c03c51c424f6c4db90
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2023-10-30 17:57:53 -0300

    corrigindo bug

--- src/pages/dynamic.tsx
+++ src/pages/dynamic.tsx
@@ -49,7 +49,7 @@ const Dynamic: NextPage = (props: {
       <Row>
         <Col>
           <h3>
-            Gerado no servidor: {props.serverSideData.timestamp.toString()}
+            Gerado no servidor: {props.serverSideData?.timestamp.toString()}
           </h3>
         </Col>
 
